8c04fbae69498e0ac285bc19685722c47211885b
SeldomseenSchweig <jamesschweig@gmail.com>
2022-10-17 14:26:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+    
+    <div className="App">
+        <h1>Hello</h1>
+
+      
+      {/* <Board nrows={10} ncols={10} chanceLightStartsOn={.5}/> */}
+    </div>
+  );
+}
+
+export default App;
